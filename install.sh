7a9c772ffc64c57776210cf3a9e47e110cb7a7d2
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-03-14 18:37:42 -0300

    Add check to verify terminal client

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-_rcFile="/home/${USER}/.bashrc"
-if [ "$0" == "zsh" ]; then
-  _rcFile="/home/${USER}/.zshrc"
-fi
+setup() {
+  echo "The RC (Run Commands) file ${1} is going to be updated"
 
-echo "source /home/${USER}/.dot-files/.aliases" >> $_rcFile 
-echo "source /home/${USER}/.dot-files/.custom" >> $_rcFile
+  echo "source /home/${USER}/.dot-files/.aliases" >> $1
+  echo "source /home/${USER}/.dot-files/.custom" >> $1
+}
+
+if [[ $SHELL == *"zsh" ]]; then
+  _rcFile="/home/${USER}/.zshrc"
+  setup $_rcFile
+elif [[ $SHELL == *"bash" ]]; then
+  _rcFile="/home/${USER}/.bashrc"
+  setup $_rcFile
+else
+  echo "The configuration won't be updated due to invalid shell client, shell: ${SHELL}"
+fi
